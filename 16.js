643788f6c8d5bb2d71c2a053373b4cee97c3330d
Harsha Maria Jose <harshamariajose.20@gmail.com>
2023-07-12 12:21:48 +0530

    completed 16

---
+++ 16.js
@@ -0,0 +1,9 @@
+const inputString = "madam";
+
+const reversedString = inputString.split("").reverse().join("");
+
+if (inputString === reversedString) {
+  console.log("YES");
+} else {
+  console.log("NO");
+}
