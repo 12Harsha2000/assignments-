322799921dcf11079ceb5f89819e106cf42bbd20
Harsha Maria Jose <harshamariajose.20@gmail.com>
2023-07-12 10:11:39 +0530

    1,2

---
+++ 1.js
@@ -0,0 +1,9 @@
+
+var name = "Harsha";
+var age = 23;
+var maritalStatus = "Single";
+
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Marital Status: " + maritalStatus);
---
+++ 2.js
@@ -0,0 +1,6 @@
+const yearOfBirth = 2000;
+const currentYear = 2023;
+
+const age = currentYear - yearOfBirth;
+
+console.log("The person's age is: " + age);
